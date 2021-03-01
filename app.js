6958ad9b7212341aed9d623b648e0d29f926a1b4
nabilashikoofa <79271432+nabilashikoofa@users.noreply.github.com>
2021-03-01 08:23:18 +0700

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const login_btn = document.querySelector("#login-btn");
+const register_btn = document.querySelector("#register-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
